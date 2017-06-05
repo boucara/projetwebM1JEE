a8f4ce99d1308b75e1978253b6fc408302171415
Tom <tom.phily@live.fr>
2017-06-05 21:17:30 +0200

    resolution d'un problème de recuperation de l'utilisateur à l'affichage de information pratique

--- src/java/servlets/ServeletInformation.java
+++ src/java/servlets/ServeletInformation.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import utilisateurs.modeles.Utilisateur;
 
 /**
  *
@@ -24,28 +25,18 @@ public class ServeletInformation extends MaServlet {
     @Override
     protected void processRequestGetCo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-         String action = request.getParameter("action");
-        String forwardTo = "information.jsp";
-        String message = "";
-        if (action != null) {
-            if (action.equals("info")) {
-                forwardTo = "information.jsp";
-                message = "Bienvenue! sur la page d'information";
-
-            }
-        }
-        
-      
-        
-        RequestDispatcher dp = request.getRequestDispatcher(forwardTo);  
-        dp.forward(request, response);
-        
-        
+        Utilisateur user = (Utilisateur) request.getSession().getAttribute("utilisateur");
+        request.setAttribute("utilisateur", user);
+        request.setAttribute("connexion", true);
+        processRequest(request, response);
 
     }
 
     @Override
     protected void processRequestPostCo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Utilisateur user = (Utilisateur) request.getSession().getAttribute("utilisateur");
+        request.setAttribute("utilisateur", user);
+        request.setAttribute("connexion", true);
         processRequest(request, response);
     }
 
@@ -72,9 +63,6 @@ public class ServeletInformation extends MaServlet {
 
             }
         }
-        
-      
-        
         RequestDispatcher dp = request.getRequestDispatcher(forwardTo);  
         dp.forward(request, response);
     }
