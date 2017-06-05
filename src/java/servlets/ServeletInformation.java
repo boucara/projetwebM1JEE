15bf411151ca566f9cadff9cb9178495a5cbc3aa
boucara <traorebelco@gmail.com>
2017-06-05 19:45:43 +0200

    finalisation page tshirt

--- src/java/servlets/ServeletInformation.java
+++ src/java/servlets/ServeletInformation.java
@@ -24,6 +24,22 @@ public class ServeletInformation extends MaServlet {
 
     @Override
     protected void processRequestGetCo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         String action = request.getParameter("action");
+        String forwardTo = "information.jsp";
+        String message = "";
+        if (action != null) {
+            if (action.equals("info")) {
+                forwardTo = "information.jsp";
+                message = "Bienvenue! sur la page d'information";
+
+            }
+        }
+        
+      
+        
+        RequestDispatcher dp = request.getRequestDispatcher(forwardTo);  
+        dp.forward(request, response);
+        
         
     }
 
